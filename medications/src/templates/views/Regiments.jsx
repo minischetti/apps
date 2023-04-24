220f9d96d8361c9726f678c6cdaf91e3dec14b7b
Dominic Minischetti III <domminischetti@gmail.com>
2023-04-23 18:45:36 -0700

    Add basic regiment functionality

--- medications/src/templates/views/Regiments.jsx
+++ medications/src/templates/views/Regiments.jsx
@@ -12,34 +12,36 @@ export function Regiments() {
 
     useEffect(() => {
         console.log('Regiments mounted');
-        console.log(medications);
-        console.log(location)
+        console.log(regiments);
         return () => {
             console.log('Regiments unmounted');
         };
-    }, []);
+    }, [regiments]);
 
     const handleAddRegiment = (event) => {
         event.preventDefault();
         const form = event.target;
 
-        // check if medication name is empty
-        if (!form.name.value) {
-            alert('Please enter a medication name.');
+        // If a regiment already exists for this medication, update it
+        if (regiments.find((regiment) => regiment.medicationId === form.id.value)) {
+            let regiment = regiments.find((regiment) => regiment.medicationId === form.id.value);
+            regiment = {
+                id: regiment.id,
+                medicationId: regiment.medicationId,
+                times: Array.from({ length: frequency }).map((_, index) => form[`time_${index}`].value),
+            };
+            // regiment.times = Array.from({ length: frequency }).map((_, index) => form[`time_${index}`].value);
+            dispatch(update(regiment));
             return;
         }
 
-        // check if medication already exists
-        if (medications.find((medication) => medication.name === form.name.value)) {
-            alert('Medication already exists.');
-            return;
-        }
-
-        const medication = {
-            id: medications.length + 1,
-            name: form.name.value,
+        const regiment = {
+            id: regiments.length + 1,
+            medicationId: form.id.value,
+            times: Array.from({ length: frequency }).map((_, index) => form[`time_${index}`].value),
         };
-        dispatch(add(medication))
+            
+        dispatch(add(regiment))
     }
 
     const removeTime = (event) => {
@@ -64,6 +66,23 @@ export function Regiments() {
     return (
         <div style={styles.container}>
             <h1>Regiments</h1>
+            {regiments && regiments.length > 0 ? (
+                <div>
+                    <p>Here you can add, remove, and edit your regiments.</p>
+                    <ul>
+                        {regiments.map((regiment) => (
+                            <li key={regiment.id}>
+                                <div>{medications.find((medication) => medication.id == regiment.medicationId).name}</div>
+                                <div>{regiment.times}</div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ) : (
+                <div>
+                    <p>You have no regiments. Add a regiment to get started.</p>
+                </div>
+            )}
             <p>Add, remove, and edit your medication regiments.</p>
             <form onSubmit={handleAddRegiment}>
                 <h2>Regiment</h2>
@@ -71,12 +90,10 @@ export function Regiments() {
                 {medications && medications.length > 0 ? (
                     <div>
                         <h3>Add a regiment</h3>
-                        <p>What medication do you want to add to your regiment?</p>
-                        <select name='name'>
+                        <p>Which medication do you want to add to your regiment?</p>
+                        <select name='id'>
                             {medications.map((medication) => (
-                                <option key={medication.id} value={medication.name}>
-                                    {medication.name}
-                                </option>
+                                <option key={medication.id} id={medication.id} value={medication.id} label={medication.name}/>
                             ))}
                         </select>
                         <p>How many times do you take this medication?</p>
@@ -84,7 +101,7 @@ export function Regiments() {
                             <h3>Times</h3>
                             <p>When do you take this medication?</p>
                             <footer>You may add multiple times.</footer>
-                            <button type="button" onClick={removeTime}>Remove a time</button>
+                            <button type="button" onClick={removeTime} disabled={frequency <= 1}>Remove a time</button>
                             <button type="button" onClick={addTime}>Add a time</button>
                             {Array.from({ length: frequency }).map((_, index) => (
                                 <input key={`time_${index}`} type='time' name={`time_${index}`} />
