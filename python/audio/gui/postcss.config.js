e6d4436ef7866dae4d0c6f9d0df8f191bb855f6a
Dominic Minischetti <domminischetti@gmail.com>
2023-06-18 13:56:23 -0700

    Update postcss config

--- python/audio/gui/postcss.config.js
+++ python/audio/gui/postcss.config.js
@@ -2,15 +2,5 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     'postcss-nested': {},
-    'postcss-preset-env': {},
-    'postcss-pxtorem': {
-      rootValue: 16,
-      unitPrecision: 5,
-      propList: ['*'],
-      selectorBlackList: ['html', 'body'],
-      replace: true,
-      mediaQuery: false,
-      minPixelValue: 0
-    }
   }
 }
